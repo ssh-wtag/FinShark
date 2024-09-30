1f19e2bc15c2382c35241041c91677233ab99b00
Sadman Shoumik <sadman.shoumik@welldev.io>
2024-09-30 13:43:25 +0600

    GET, POST, PUT, DELETE
    Implemented GET, POST, PUT, DELETE within the Project to Modify Data in the Stocks Table

--- FinShark/api/Controllers/StockController.cs
+++ FinShark/api/Controllers/StockController.cs
@@ -41,12 +41,47 @@ namespace api.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create([FromBody] CreateStockRequestDTO createStockRequestDTO)
+        public IActionResult Create([FromBody] CreateStockRequestDTO createDTO)
         {
-            var newStock = createStockRequestDTO.ToStockFromCreateStockRequestDTO();
+            var newStock = createDTO.ToStockFromCreateStockRequestDTO();
             _context.Stocks.Add(newStock);
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetById), new { id = newStock.Id}, newStock.ToStockDTOFromStock() );
+        }
+
+        [HttpPut] // [HttpPut("{id}")]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDTO updateDTO)
+        {
+            var oldStock = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if(oldStock == null)
+                return NotFound();
+
+            oldStock.Symbol = updateDTO.Symbol;
+            oldStock.CompanyName = updateDTO.CompanyName;
+            oldStock.Purchase = updateDTO.Purchase;
+            oldStock.LastDiv = updateDTO.LastDiv;
+            oldStock.Industry = updateDTO.Industry;
+            oldStock.MarketCap = updateDTO.MarketCap;
+
+            _context.SaveChanges();
+
+            return Ok(oldStock.ToStockDTOFromStock());
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if(stock == null)
+                return NotFound();
+
+            _context.Stocks.Remove(stock);
+            _context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
